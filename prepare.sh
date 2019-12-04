1a56342de5ddc9aa69868125aee4e2e998c581ab
jaedeok <05jaedeok@gmail.com>
2019-12-03 22:03:33 -0500

    Fix preparation script

--- prepare.sh
+++ prepare.sh
@@ -3,9 +3,9 @@
 mkdir -p data
 cd data
 echo "Downloading data at `pwd`"
-base_url = "https://physics.bu.edu/~pankajm/ML-Review-Datasets/isingMC/"
+base_url="https://physics.bu.edu/~pankajm/ML-Review-Datasets/isingMC"
 wget "$base_url/Ising2DFM_reSample_L40_T=All.pkl"
-wget "Ising2DFM_reSample_L40_T=All_labels.pkl	"
+wget "$base_url/Ising2DFM_reSample_L40_T=All_labels.pkl"
 cd ..
 
 echo "Done"
